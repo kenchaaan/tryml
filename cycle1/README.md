68744fadc4e4a45b9f38a2a86669dc8abd9bd679
kenji kondo <kenji kondo>
2018-09-30 15:38:40 +0900

    add checks

--- cycle1/README.md
+++ cycle1/README.md
@@ -1,13 +1,16 @@
-# play examples
+# 確認
 
 ```python
-from cycle1 import examples as ex
-ex.out_normal_data_to_csv('./cycle1/resources/male.csv', 1000, 172.14, 5.57)
-ex.plot_data_from_csv('./cycle1/resources/male.csv')
-```
+from cycle1 import checks as ck
+# check 1
+ck.show_normal_dist()
 
-# play practices
+# check 2
+ck.solve_2dim_equations(a=3, b=0, c=0, d=2, i=12, j=26)
 
-```python
-from cycle1 import practices as pr
+# check 3
+ck.out_normal_data_to_csv(filepath='./cycle1/resources/male.csv', num=1000, mu=172.14, sigma=5.57)
+
+# check 4
+ck.plot_data_from_csv(filepath='./cycle1/resources/male.csv')
 ```
--- cycle1/checks.py
+++ cycle1/checks.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 
 def show_normal_dist():
-    # show the graph of the density function of normal dist.
     x = np.arange(-4, 4, 0.1)
     y = st.norm.pdf(x, loc=0, scale=1)
     plt.plot(x, y)
@@ -13,23 +12,20 @@ def show_normal_dist():
 
 
 def solve_2dim_equations(a, b, c, d, i, j):
-    # solve simultaneous equation on 2 dim.
     mat = np.array([[a, b], [c, d]])
     vec = np.array([i, j])
     return np.linalg.solve(mat, vec)
 
 
 def out_normal_data_to_csv(filepath, num, mu, sigma):
-    # out data according to normal distribution to "filepath"
-    x = pd.DataFrame(columns=['height'])
-    x['height'] = sigma * np.random.randn(num) + mu
+    x = pd.DataFrame(columns=['tall'])
+    x['tall'] = sigma * np.random.randn(num) + mu
     with open(filepath, 'w') as outfile:
-        x.to_csv(outfile, header=False)
+        x.to_csv(outfile, index_label='index')
 
 
 def plot_data_from_csv(filepath):
-    # plot data from csv
     with open(filepath, 'r') as f:
-        x = pd.read_csv(f, header=None, names=('index', 'height'))
-    plt.plot(x['height'].values, "o")
+        x = pd.read_csv(f)
+    plt.plot(x['index'], x["tall"], "o")
     plt.show()
